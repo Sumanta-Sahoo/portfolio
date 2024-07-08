9396159805f8f00dfa72636479bdea5cbdfd8a74
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-07-08 18:30:36 +0530

    Updating social links

--- src/Components/navabr/Navbar.jsx
+++ src/Components/navabr/Navbar.jsx
@@ -19,17 +19,17 @@ const Navbar = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: .5 }}>
 
-          <a href="https://www.linkedin.com/in/sumanta-s15/" target="blank"><img src="/linkedin.png" alt="linkedin Social Link" /></a>
+          <a href="https://linkedin.com/in/sumanta-s14" target="blank"><img src="/linkedin.png" alt="linkedin Social Link" /></a>
           <a href="https://github.com/Sumanta-Sahoo" target="blank"><img src="/github.png" alt="github Social Link" /></a>
           <a href="https://dribbble.com/Sumant_S15" target="blank"><img src="/dribbble.png" alt="dribbble Social Link" /></a>
-          <a href="#" target="blank"><img src="/instagram.png" alt="Dribble Social Link" /></a>
+          <a href="https://www.instagram.com/success_saga___/?igsh=ZWQ3OGFuOWF4aDZl" target="blank"><img src="/instagram.png" alt="Dribble Social Link" /></a>
         </motion.div>
         <motion.div className="resumeButton"
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: .5 }}>
           <motion.button>
-            <a href="https://docs.google.com/document/d/1JdSs0frlU1DsW-az60VkXytnN5EuDgYbtEzgN4NfjM4/view" target="blank">View Resume</a>
+            <a href="https://drive.google.com/file/d/1m3-9MBcLhFLetsXXJGxQWOD-Jkj2nH6c/view?usp=sharing" target="blank">View Resume</a>
           </motion.button>
         </motion.div>
       </div>
